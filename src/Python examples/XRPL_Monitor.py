d07cf89ea089e5e828d9fb9e9640aeeadd016ad3
Forex786 <Forex786@hotmail.co.uk>
2023-12-02 11:24:25 +0000

    SQL updates

--- src/Python examples/XRPL_Monitor.py
+++ src/Python examples/XRPL_Monitor.py
@@ -6,8 +6,18 @@
 import ast
 from datetime import datetime
 import xrpl
+from xrpl.utils import hex_to_str, str_to_hex
 import asyncio
 from threading import Thread
+from icecream import ic
+import logging
+# logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+# logging.debug('This message should go to the log file')
+# logging.info('So should this')
+# logging.warning('And this, too')
+# logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+### my libs
 from class_mySQL  import MySQLConnector
 
 class XRPLMonitorThread(Thread):
@@ -19,14 +29,17 @@ class XRPLMonitorThread(Thread):
 
     Tx_count = 0
 
-    def __init__(self, url):
+    def __init__(self, url ):
         Thread.__init__(self, daemon=True)
-      
+
+        #self.mydb = mydb
         self.url = url
         self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-        self.loop.set_debug(True)
+        #self.loop.set_debug(True)
+        self.loop.set_debug(False)
 
+        asyncio.set_event_loop(self.loop)
+        
     # this is a default Thread function which is overidden here. It is executed by the Thread.start method.
     def run(self):
         """
@@ -34,6 +47,60 @@ class XRPLMonitorThread(Thread):
         from the XRPL.
         """
         self.loop.run_forever()
+
+    def saveToDB(self, message):
+        # TODO: save the following meta data to dB.
+
+        try:
+            tx_hash = message["transaction"]["hash"]
+                    
+            txLedger = message['transaction']['LastLedgerSequence']
+            memos = message["transaction"]["Memos"] # list of dictionaries
+            Memo = memos[0]  # get the first
+            MemoData = hex_to_str(Memo['Memo']['MemoData'])
+            MemoType = hex_to_str(Memo['Memo']['MemoType'])
+            dTag = message['transaction']['DestinationTag']
+            destAcc = message['transaction']['Destination']
+            txResult = message["engine_result"]
+            ledgerValidated = message["validated"]
+
+            logging.info(f"Saving to dB dTag: {dTag}")
+            
+           
+
+            # ic(tx_hash,txLedger,MemoType,MemoData,dTag,destAcc )
+            
+            """
+            message['transaction']: {'Account': 'rUxi1XtG7521a8eHH6gzcaMXBE6bCAzjqm',
+                        'Amount': '10000',
+                        'Destination': 'ra7NXCfrANoLcKJKKWuFFyQMDMNPLfuTFL',
+                        'DestinationTag': 1000002,
+                        'Fee': '10',
+                        'Flags': 0,
+                        'LastLedgerSequence': 43356720,
+                        'Memos': [{'Memo': {'MemoData': '50726546756E6420637265646974',
+                                            'MemoType': '63617368585250'}}],
+                        'Sequence': 42376130,
+                        'SigningPubKey': 'ED41C26F4C4DF7DC7ADA6DC3C5C33686FDA40F2043FFE55D337EBAEDC3DEAB9F10',
+                        'TransactionType': 'Payment',
+                        'TxnSignature': 'CD1BF58F6CB45396E18C941A13A9CB42E6BE059084C01CFB7EAF77E0CD8C8C9327AD786EB0D5CA0742E978C3DDD55E931B0ADA20D535BD90579533091D341D08',
+                        'date': 754745321,
+                        'hash': '12A078DC2465E8CF04509BB9E927D7014527032E8D2E7BD4B5D6F7AFD16D92DD'} 
+
+            """
+            
+            result = mydb.saveValidatedTx (
+                                dTag 		,
+                                MemoType 	,
+                                MemoData 	,
+                                tx_hash 		,
+                                destAcc,
+                                txLedger 	,
+                                txResult 	,
+                                ledgerValidated )
+            
+        except Exception as e:
+            ic("SaveToDB:", e)                      
 
     async def watch_xrpl_account(self, addresses, wallet=None):
         """
@@ -59,13 +126,20 @@ class XRPLMonitorThread(Thread):
 
                     # message contains the status of the tx & ledger and Tx details
                     print(datetime.now().strftime("%H:%M:%S"),"-",
+                          message['transaction']['LastLedgerSequence'],"-",
                           message['transaction']['DestinationTag'],"-",
-                          message["transaction"]["hash"], 
                           ":",message["engine_result"],
-                          ":",message["validated"]
+                          ":",message["validated"],
+                          message["transaction"]["hash"]
                           )
                     self.Tx_count = self.Tx_count + 1
 
+                    try:
+                        self.saveToDB(message)
+                    except Exception as e:
+                        logging.info(f"Issue saving to dB : tag: {message['transaction']['DestinationTag']}")
+                        logging.info(e)
+                        ic(e)   
                     
 
     async def on_connected(self):
@@ -92,26 +166,42 @@ async def main():
     # non-GUI program.
     # This loop emulates the GUI main loop.
     while True:
-        print (datetime.now().strftime("%H:%M:%S"), " : Monitoring the XRPL....total Tx count ==>>", worker.Tx_count)
+        print ("\n",datetime.now().strftime("%H:%M:%S"), " : Monitoring the XRPL....total Tx count ==>>", worker.Tx_count)
+        print ("\nexecTime   Ledger#    Tx TAG     Tx result  Validated?   Tx HASH\n")
+       #        11:37:36 - 43356893 - 1000001 - : tesSUCCESS : True B6F94A2D3D3AEA
         await asyncio.sleep(60)
 
 ##########################################################################################################
 
 if __name__ == "__main__":
 
-    print ("\n **** v1.3 ********  Press CTRL+c to eixt  ************\n")
+    logging.basicConfig(filename='XRPL_Monitor.log', encoding='utf-8', level=logging.DEBUG)
+    # Truncate the log files
+    with open('XRPL_Monitor.log', 'w'):
+        pass
+
+    websockets_logger = logging.getLogger("websockets")
+    websockets_logger.setLevel(logging.INFO)
+
+    logging.info(f"Daemon - Monitor XRPL Tx's started at , {datetime.now().strftime('%Y:%M:%d %H:%M:%S')} \n\n")
+
+    print ("\n\n\n\n\n **** v1.8 ********  Press CTRL+c to eixt  ************\n")
 
     print ("Daemon - Monitor XRPL Tx's started at ", datetime.now().strftime("%Y:%M:%d %H:%M:%S"), "\n")
 
     WS_URL = "wss://s.altnet.rippletest.net:51233" # Testnet
     print("XRPL network : ", WS_URL, "\n")
     
-    # Start background thread for updates from the ledger ------------------
-    worker = XRPLMonitorThread(WS_URL) # creates an endless worker loop
-    worker.start()
-
+    
     try:
         mydb = MySQLConnector()
+        mydb.truncate_Table("Txs_Pending")
+        mydb.truncate_Table("Txs_Validated")
+        
+
+        # Start background thread for updates from the ledger ------------------
+        worker = XRPLMonitorThread(WS_URL) # creates an endless worker loop
+        worker.start()
 
         result = mydb.get_XRPL_Wallets_to_Monitor()
 
@@ -147,7 +237,7 @@ if __name__ == "__main__":
             print ("mySQL dB connection closed.")
     
     except:
-        print ("mySQL dB error...")
+        print ("__name__ :  mySQL dB error...")
         mydb.close_connection()
         
 
